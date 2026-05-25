8b3187611bf1aab49b16a34d009ba9e6371a68a6
Tefo Leche <tefoleche03@gmail.com>
2026-05-25 23:21:36 +0200

    Added themes file to Javascript folder

---
+++ Javascript-Certification/javascript-trivia-bot/script.js
@@ -0,0 +1,21 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+const botName = "Tefobot";
+const botLocation = "Mars";
+const favoriteLanguage = "Rust";
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact = favoriteLanguage + " is a cool but challenging to learn low level language that is highly acclaimed for it's performance speed and memory safety.\n"
+
+console.log(codingFact);
+
+codingFact = "Another impressive fact about " + favoriteLanguage + " is that it does all of this without needing a garbage collector.\n"
+console.log(codingFact);
+
+codingFact = "As great as " + favoriteLanguage + "is, beware! You will spend quite a bit of time fighting it's Borrow Checker, at least at the beginning of your learning journey.\n"
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + "."); 
