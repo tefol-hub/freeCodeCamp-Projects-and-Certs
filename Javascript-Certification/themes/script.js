5414528be9cb01ee0a78bfcc72a1b56aa62024c2
Tefo Leche <tefoleche03@gmail.com>
2026-05-27 00:34:21 +0200

    Update language label format in toolbar

--- Javascript-Certification/themes/script.js
+++ Javascript-Certification/themes/script.js
@@ -12,7 +12,7 @@ fetch('script.js')
 Prism.plugins.toolbar.registerButton("lang-label", function (env) {
   const span = document.createElement("span");
 
-  span.textContent = `${env.language}`;
+  span.textContent = `</> ${env.language}`;
 
   span.classList.add("lang-label");
   
